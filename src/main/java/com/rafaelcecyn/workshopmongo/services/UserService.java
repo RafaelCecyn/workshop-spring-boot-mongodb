8de22fc84a95db344bb9a4c3bc905aa34769e88a
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-01-31 14:32:37 -0300

    Inserção de usuário com POST

--- src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelcecyn.workshopmongo.domain.User;
+import com.rafaelcecyn.workshopmongo.dto.UserDTO;
 import com.rafaelcecyn.workshopmongo.repository.UserRepository;
 import com.rafaelcecyn.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(),obj.getName(),obj.getEmail());
 	}
 	
 }
