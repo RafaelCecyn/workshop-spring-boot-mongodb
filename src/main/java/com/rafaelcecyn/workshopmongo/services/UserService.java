770a9f6259396aec9c0d87ab06bf63e05ada9170
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-01-31 14:36:24 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
@@ -35,4 +35,9 @@ public class UserService {
 		return new User(obj.getId(),obj.getName(),obj.getEmail());
 	}
 	
+	public void delete(String id) {
+		findById(id); // Verifica se existe Id
+		repo.deleteById(id);
+	}
+	
 }
