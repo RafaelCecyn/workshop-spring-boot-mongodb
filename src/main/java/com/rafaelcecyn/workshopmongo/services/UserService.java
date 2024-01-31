013c9e88e345b0ea008d751da29f26b1358f4f53
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-01-31 09:57:08 -0300

    Obtendo um usuário por id

--- src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelcecyn/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rafaelcecyn.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelcecyn.workshopmongo.domain.User;
 import com.rafaelcecyn.workshopmongo.repository.UserRepository;
+import com.rafaelcecyn.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,11 @@ public class UserService {
 	
 	public List<User> findall() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
 	
 }
