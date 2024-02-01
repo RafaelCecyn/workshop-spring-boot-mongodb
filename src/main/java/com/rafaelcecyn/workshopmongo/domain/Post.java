51c4791444e681a79aa5fbcaab4fd85afe6fed15
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-02-01 14:20:26 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rafaelcecyn/workshopmongo/domain/Post.java
+++ src/main/java/com/rafaelcecyn/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.rafaelcecyn.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rafaelcecyn.workshopmongo.dto.AuthorDTO;
+import com.rafaelcecyn.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comment = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,6 +101,8 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 	
 	
 	
